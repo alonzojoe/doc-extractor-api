de52806c82a00523294e20719e88c5fe2e95a1b3
alonzojoe <alonzojoenell2k15@gmail.com>
2026-01-28 22:04:52 +0800

    feat: collate created routes in index file, implement helment, cors and rateLimit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,72 @@
 const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+const rateLimit = require("express-rate-limit")
+const fs = require("fs")
 require("dotenv").config();
 
+const config = require("./config")
+const documentRoutes = require("./routes/document.routes")
+
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = config.port
+
+if(!fs.existsSync('uploads')) {
+    fs.mkdirSync('uploads')
+}
+
+app.use(helmet())
+
+app.use(cors({
+    origin: config.frontendUrl,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests.'
+})
+
+app.use(limiter)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (_, res) => {
-    res.send({
-        message: 'Home'
+    res.json({
+        message: 'Document Extractor POC API',
+        version: '1.0.0',
+        endpoints: {
+            health: '/api/documents/health',
+            extract: 'POST /api/documents/extract'
+        }
     })
 })
 
+app.use('/api/documents', documentRoutes)
+
+app.use((_, res) => {
+  res.status(404).json({
+    error: 'Not Found',
+    message: 'The requested resource was not found'
+  });
+});
+
+app.use((err, req, res, next) => {
+    console.log('Error: ', err)
+
+    if (err.message.includes('File too large')) {
+        return res.status(413).json({
+        error: 'Payload Too Large',
+        message: 'File size exceeds the maximum allowed size'
+        });
+    }
+})
+
 app.listen(PORT, () => {
-    console.log(`Server is listening to port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
+    console.log(`Environment: ${config.nodeEnv}`);
+    console.log(`CORS enabled for: ${config.frontendUrl}`);
 })
